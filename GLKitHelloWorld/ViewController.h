ccdd46502f93c38da417ecf8951fbc07b6dbff0e
ileider <ileider@gmail.com>
2011-10-20 19:44:13 +0200

    Initial Commit

---
+++ GLKitHelloWorld/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GLKitHelloWorld
+//
+//  Created by Ivan Leider on 20/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
